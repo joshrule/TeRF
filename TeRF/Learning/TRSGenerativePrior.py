5d5c5ed079b2ab77282d3324d70af8d0a4b1491b
Josh Rule <rule@mit.edu>
2017-07-05 15:30:15 -0400

    reconcile prior and likelihood with earlier refactor

--- TeRF/Learning/TRSGenerativePrior.py
+++ TeRF/Learning/TRSGenerativePrior.py
@@ -1,20 +1,19 @@
 from LOTlib.Miscellaneous import attrmem
 from scipy.stats import geom
 
-from TeRF.Utilities import log_p
+from TeRF.Types import Signature
 
 
 class TRSGenerativePrior(object):
-    
+
     @attrmem('prior')
     def compute_prior(self):
         """
         the log prior of the hypothesis
 
-        This prior assumes a way to sample the TRS from wholecloth. Sample some
-        number of operators, each with their arity, and then some number of
-        variables. Finally, sample some number of rules and sample trees for
-        each rule.
+        This prior assumes a way to sample the TRS from scratch. Sample some
+        number of operators, each with their arity, and then sample some number
+        of rules and their constituent LHS and RHS(s).
 
         Given the assumptions below, it should be part of a TRSHypothesis.
 
@@ -23,22 +22,19 @@ class TRSGenerativePrior(object):
           self.prior_temperature: from LOTlib.Hypotheses.Hypothesis
           self.p_operators: should be an argument to hypothesis' __init__
           self.p_arity: should be an argument to hypothesis' __init__
-          self.p_variables: should be an argument to hypothesis' __init__
           self.p_rules: should be an argument to hypothesis' __init__
         Returns: a float, -inf <= x <= 0, log p(self.value)
         """
-        n_operators = len(self.value.operators)+1
-        n_variables = len(self.value.variables)+1
-        total_arity = sum([op.arity+1 for op in self.value.operators])
-        n_rules = len(self.value.rules)+1
+        trs = self.value
+        n_operators = len(trs.operators)+1
+        total_arity = sum([op.arity+1 for op in trs.operators])
+        n_rules = len(trs.rules)+1
         p_rules = 0.0
         for r in self.value.rules:
-            p_rules += log_p(r.lhs, self.value.operators |
-                             self.value.variables)
-            rhs_signature = self.value.operators | r.lhs.variables()
-            p_rules += log_p(r.rhs, rhs_signature)
+            p_rules += Signature(trs.operators).log_p(r.lhs, invent=True)
+            rhs_signature = Signature(trs.operators | r.lhs.variables())
+            p_rules += [rhs_signature.log_p(t, invent=False) for t in r.rhs]
         return (geom.logpmf(k=n_operators, p=self.p_operators) +
                 geom.logpmf(k=total_arity, p=self.p_arity) +
-                geom.logpmf(k=n_variables, p=self.p_variables) +
                 geom.logpmf(k=n_rules, p=self.p_rules) +
                 p_rules) / self.prior_temperature
--- TeRF/Learning/TRSLikelihood.py
+++ TeRF/Learning/TRSLikelihood.py
@@ -1,6 +1,4 @@
-from scipy.stats import geom, bernoulli
-
-from TeRF.Utilities import log_p, rewrites_to
+from TeRF.Types.Trace import rewrites_to
 
 
 class TRSLikelihood(object):
@@ -9,32 +7,23 @@ class TRSLikelihood(object):
         the log likelihood of the hypothesis for a single datum
 
         This likelihood is generative. It asks how likely datum.lhs is to be
-        sampled from the value's signature and how likely datum.rhs is to be
-        observed as a successor to datum.lhs given the rules, our assumptions
-        about observation frequency, and how similar two trees need to be to
-        count as an observation.
+        generated from self.start and how likely datum.rhs is to be generated
+        from datum.lhs given the rules and our assumptions about observation
+        frequency.
 
         It ignores self.likelihood_temperature; compute_likelihood manages it.
 
         Assumes:
           self.value: from LOTlib.Hypotheses.Hypothesis
           self.p_observe: should be an argument to the hypothesis __init__
-          self.p_similar: should be an argument to the hypothesis __init__
+          self.start: the starting term
         Args:
           datum: a rewriteRule representing a single datum
         Returns: a float, -inf <= x <= 0, log p(datum | self.value)
         """
-        p_gen_lhs = log_p(datum.lhs, self.value.operators)
-        min_step, min_dist, nf_step = rewrites_to(self.value,
-                                                  datum.lhs,
-                                                  datum.rhs,
-                                                  steps=10)
-        if min_step == nf_step:
-            p_eval_rhs = geom.logsf(k=min_step, p=self.p_observe) - \
-                geom.logcdf(k=10, p=self.p_observe)  # truncated geometric
-        else:
-            p_eval_rhs = geom.logpmf(k=min_step+1, p=self.p_observe) - \
-                geom.logcdf(k=10, p=self.p_observe)  # truncated geometric
-            # p_distance = geom.logpmf(k=min_dist+1, p=self.p_similar)
-        p_distance = bernoulli.logpmf(k=min_dist, p=(1-self.p_similar))
-        return p_gen_lhs + p_eval_rhs + p_distance
+        lhs = datum.lhs
+        rhs = datum.rhs
+        start = self.start
+        p_lhs = rewrites_to(self.value, lhs, start, self.p_observe, steps=10)
+        p_rhs = rewrites_to(self.value, rhs, lhs, self.p_observe, steps=10)
+        return p_lhs + p_rhs
