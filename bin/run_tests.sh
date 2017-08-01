91f302965e4d2f6479d50c6157f96018f24f9c6f
Josh Rule <rule@mit.edu>
2017-08-01 10:37:33 -0400

    refactoring the testing code

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-import="import TeRF.Hypotheses.TRSHypothesis as t ; "
-nSamps="3200"
-today=`date '+%Y%m%d'`
-
-for i in `seq -f %03g 21 32`;
+for i in `seq -f %03g 1 32`;
 do
-    logfile=test_log_${nSamps}_${i}_20_40_3
-    testfile=library/simple_tree_manipulations/${i}.terf
-    echo Next Test $i: $testfile
-    read -p "Run this test (<y>|n)? " runit
-    if [[ $runit == '' ]] || [[ $runit == 'y' ]]
-    then
-        script $logfile bin/test.sh $nSamps $testfile $start
-    else
-        echo skipping test
-    fi
+    bin/test.sh $i $1 $2 $3
 done
--- bin/test.sh
+++ bin/test.sh
@@ -1,1 +1,17 @@
-time python TeRF/Learning/TRSHypothesis.py $1 $2 $3
+i=$1
+nSamps=$2
+start=$3
+n=$4
+testfile=library/simple_tree_manipulations/${i}.terf
+today=`date '+%Y%m%d'`
+logfile=test_log_${nSamps}_${i}_20_40_${n}
+
+echo Next Test $i: $testfile
+read -p "Run this test (<y>|n)? " runit
+if [[ $runit == '' ]] || [[ $runit == 'y' ]]
+then
+    script $logfile time python TeRF/Learning/TRSHypothesis.py $nSamps \
+           $testfile $start
+else
+    echo skipping test
+fi
