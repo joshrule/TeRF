af87b42198ac4c0df00cdbe9c84856fd71e4b9ed
Josh Rule <rule@mit.edu>
2017-09-18 13:40:09 -0400

    revert to soft likelihood

--- TeRF/Learning/TRSHypothesis.py
+++ TeRF/Learning/TRSHypothesis.py
@@ -14,8 +14,7 @@ class TRSHypothesis(GenerativePrior, Likelihood, TestProposer, Hypothesis):
 
     Args:
       data: a list of Rules to explain
-      p_observe: rate of observation (in timesteps)
-      p_similar: rate of noisiness in evaluation (in timesteps)
+      p_partial: partial credit for an incorrect answer
       p_operators: probability of adding a new operator
       p_arity: probability of increasing the arity of an operator
       p_rules: probability of adding a new rule
@@ -23,6 +22,7 @@ class TRSHypothesis(GenerativePrior, Likelihood, TestProposer, Hypothesis):
       proposers: the components of the mixture
       weights: the weights of the mixture components
     """
-    def __init__(self, **kwargs):
+    def __init__(self, temperature=1.0, **kwargs):
+        self.temperature = temperature
         self.__dict__.update(kwargs)
         super(TRSHypothesis, self).__init__(**kwargs)
