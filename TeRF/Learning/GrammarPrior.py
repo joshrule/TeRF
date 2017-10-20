3ebbf42530c174c5a5213f54fbc1f3e0826719c1
Josh Rule <rule@mit.edu>
2017-10-20 15:12:04 -0400

    swap priors (no backend yet, though)

---
+++ TeRF/Learning/GrammarPrior.py
@@ -0,0 +1,33 @@
+import LOTlib.Miscellaneous as misc
+import GrammarSampler
+
+
+class Prior(object):
+    @misc.attrmem('prior')
+    def compute_prior(self):
+        """
+        the log prior of the hypothesis' semantic grammar.
+
+        Notes
+        -----
+        - This prior focuses exclusively on the semantic grammar but should be
+          adapted as learning increases to consider the other grammars. It
+          should assume a way to sample the LOT from scratch by first sampling
+          the primitive grammar, then the syntax, then the semantics.
+
+        Given the assumptions below, it should be part of a TeRF LOTHypothesis.
+
+        Assumes
+        -------
+          self.value: from LOTlib.Hypotheses.Hypothesis
+          self.prior_temperature: from LOTlib.Hypotheses.Hypothesis
+          self.p_rules: should be an argument to hypothesis' __init__
+        Returns
+        -------
+        float
+            -inf <= x <= 0, ln p(self.value)
+        """
+        raw_prior = GrammarSampler.log_p_g(source=self.value.syntax,
+                                           target=self.value.semantics,
+                                           p_rule=self.p_rules)
+        return raw_prior / self.prior_temperature
