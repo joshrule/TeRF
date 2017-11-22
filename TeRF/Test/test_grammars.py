55feba90f9e4d18b3399bfb511445cbaa07a19e0
Josh Rule <rule@mit.edu>
2017-11-22 13:39:55 -0500

    setup the new TRS and LOT classes

--- TeRF/Test/test_grammars.py
+++ TeRF/Test/test_grammars.py
@@ -1,161 +1,166 @@
-import TeRF.Types.Application as A
-import TeRF.Types.CFGrammar as CFG
-import TeRF.Types.Grammar as G
-import TeRF.Types.LOT as LOT
-import TeRF.Types.Operator as Op
-import TeRF.Types.Rule as R
-import TeRF.Types.Variable as V
+# import TeRF.Types.Application as A
+# import TeRF.Types.CFGrammar as CFG
+# import TeRF.Types.Grammar as G
+# import TeRF.Types.LOT as LOT
+# import TeRF.Types.Operator as Op
+# import TeRF.Types.Rule as R
+# import TeRF.Types.Variable as V
 
 
-def f(x, xs=None):
-    if xs is None:
-        xs = []
-    return A.App(x, xs)
+types = 'types'
+primitives = 'primitives'
+semantics = 'semantics'
 
 
-def g(x, y):
-    return A.App(DOT, [x, y])
-
-
-# make a bunch of primitives
-START = Op.Operator('START', 0)
-LIST = Op.Operator('list', 0)
-NUMBER = Op.Operator('number', 0)
-NIL = Op.Operator('nil', 0)
-NEL = Op.Operator('nel', 0)
-CONS = Op.Operator('cons', 0)
-ZERO = Op.Operator('0', 0)
-ONE = Op.Operator('1', 0)
-TWO = Op.Operator('2', 0)
-THREE = Op.Operator('3', 0)
-DOT = Op.Operator('.', 2)
-HEAD = Op.Operator('head', 0)
-TAIL = Op.Operator('tail', 0)
-NUMBERS = [Op.Operator(str(i), 0) for i in xrange(100)]
-
-# make a few variables
-X = V.Var('x')
-Y = V.Var('y')
-Z = V.Var('z')
-
-# make an fPCFG for just the minimal list primitives and 0-3
-list_fpcfg = CFG.FPCFG(primitives={START, LIST, NIL, CONS, DOT,
-                                   NUMBER, ZERO, ONE, TWO, THREE})
-
-# make an fPCFG for list primitives, including head
-head_fpcfg = CFG.FPCFG(primitives={START, LIST, NUMBER, NIL, NEL, CONS,
-                                   ZERO, ONE, TWO, THREE, DOT, HEAD},
-                       start=f(START))
-
-# make an fPCFG for list primitives, including head & tail
-headtail_fpcfg = CFG.FPCFG(primitives={START, LIST, NUMBER, NIL, NEL, CONS,
-                                       ZERO, ONE, TWO, THREE, DOT, HEAD, TAIL},
-                           start=f(START))
-
-# make a PCFG for learning the syntax of lists
-rule_START = R.Rule(f(START),
-                    {f(NUMBER), f(ZERO), f(ONE), f(TWO), f(THREE),
-                     f(LIST), f(NIL), f(CONS), g(f(START), f(START))})
-
-list_pcfg = CFG.PCFG(rules={rule_START})
-
-# make a PCFG for the head list problem
-rule_START = R.Rule(f(START), {f(LIST), f(NUMBER)})
-rule_LIST = R.Rule(f(LIST), {f(NIL), f(NEL)})
-rule_NEL = R.Rule(f(NEL), g(g(f(CONS), f(NUMBER)), f(LIST)))
-rule_NUMBER = R.Rule(f(NUMBER), {f(ZERO), f(ONE), f(TWO), f(THREE)})
-rule_HEAD = R.Rule(f(NUMBER), g(f(HEAD), f(NEL)))
-
-head_pcfg = CFG.PCFG(rules={rule_START, rule_LIST, rule_NEL,
-                            rule_NUMBER, rule_HEAD},
-                     start=f(START), locked=False)
-
-# make a PCFG for the head/tail problem
-rule_TAIL = R.Rule(f(LIST), g(f(TAIL), f(LIST)))
-
-headtail_pcfg = CFG.PCFG(rules={rule_START, rule_LIST, rule_NEL,
-                                rule_NUMBER, rule_HEAD, rule_TAIL},
-                         start=f(START), locked=False)
-
-# make a Grammar for list semantics
-rule_LIST = R.Rule(f(LIST), {f(NIL), g(g(f(CONS), f(NUMBER)), f(LIST))})
-
-list_g = G.Grammar(rules={rule_NUMBER, rule_LIST})
-
-# make a Grammar for head semantics
-head_lhs = g(f(HEAD), g(g(f(CONS), X), Y))
-head_rhs = X
-head_rule = R.Rule(head_lhs, head_rhs)
-
-head_g = G.Grammar(rules={head_rule}, locked=False)
-
-# make a Grammar for head/tail semantics
-tail_lhs1 = g(f(TAIL), f(NIL))
-tail_rhs1 = f(NIL)
-tail_lhs2 = g(f(TAIL), g(g(f(CONS), X), Y))
-tail_rhs2 = Y
-rule_TAIL_1 = R.Rule(tail_lhs1, tail_rhs1)
-rule_TAIL_2 = R.Rule(tail_lhs2, tail_rhs2)
-headtail_g = G.Grammar(rules={head_rule, rule_TAIL_1, rule_TAIL_2},
-                       locked=False)
-
-# make an LOT for learning to make lists
-list_lot = LOT.LOT(primitives=list_fpcfg, syntax=list_pcfg, semantics=list_g)
-
-# make an LOT for learning head
-head_lot = LOT.LOT(primitives=head_fpcfg, syntax=head_pcfg, semantics=head_g)
-
-# make an LOT for learning head/tail
-headtail_lot = LOT.LOT(primitives=headtail_fpcfg,
-                       syntax=headtail_pcfg,
-                       semantics=headtail_g)
-
-# make some test data
-head_datum_T = R.Rule(g(f(HEAD),
-                        g(g(f(CONS), f(ZERO)),
-                          g(g(f(CONS), f(ONE)),
-                            g(g(f(CONS), f(TWO)),
-                              g(g(f(CONS), f(THREE)),
-                                f(NIL)))))),
-                      f(ZERO))
-
-head_datum_T2 = R.Rule(g(f(HEAD),
-                         g(g(f(CONS), f(ONE)),
-                           g(g(f(CONS), f(THREE)),
-                             g(g(f(CONS), f(TWO)),
-                               f(NIL))))),
-                       f(ONE))
-
-head_datum_T3 = R.Rule(g(f(HEAD),
-                         g(g(f(CONS), f(TWO)),
-                           f(NIL))),
-                       f(TWO))
-
-head_datum_F = R.Rule(g(f(HEAD),
-                        g(g(f(CONS), f(ZERO)),
-                          g(g(f(CONS), f(ONE)),
-                            g(g(f(CONS), f(TWO)),
-                              g(g(f(CONS), f(THREE)),
-                                f(NIL)))))),
-                      f(NIL))
-
-list_datum_T = R.Rule(f(LIST),
-                      g(g(f(CONS), f(ZERO)),
-                        g(g(f(CONS), f(ONE)),
-                          g(g(f(CONS), f(TWO)),
-                            f(NIL)))))
-
-list_datum_F = R.Rule(f(LIST),
-                      g(g(f(ONE), f(ZERO)),
-                        f(NIL)))
-
-# full head syntax
-rule_START = R.Rule(f(START), {f(LIST), f(NUMBER)})
-rule_LIST = R.Rule(f(LIST), {f(NIL), f(NEL)})
-rule_NEL = R.Rule(f(NEL), g(g(f(CONS), f(NUMBER)), f(LIST)))
-rule_NUMBER = R.Rule(f(NUMBER), {f(N) for N in NUMBERS})
-rule_HEAD = R.Rule(f(NUMBER), g(f(HEAD), f(NEL)))
-
-full_head_pcfg = CFG.PCFG(rules={rule_START, rule_LIST, rule_NEL,
-                                 rule_NUMBER, rule_HEAD},
-                          start=f(START), locked=False)
+# def f(x, xs=None):
+#     if xs is None:
+#         xs = []
+#     return A.App(x, xs)
+#
+#
+# def g(x, y):
+#     return A.App(DOT, [x, y])
+#
+#
+# # make a bunch of primitives
+# START = Op.Operator('START', 0)
+# LIST = Op.Operator('list', 0)
+# NUMBER = Op.Operator('number', 0)
+# NIL = Op.Operator('nil', 0)
+# NEL = Op.Operator('nel', 0)
+# CONS = Op.Operator('cons', 0)
+# ZERO = Op.Operator('0', 0)
+# ONE = Op.Operator('1', 0)
+# TWO = Op.Operator('2', 0)
+# THREE = Op.Operator('3', 0)
+# DOT = Op.Operator('.', 2)
+# HEAD = Op.Operator('head', 0)
+# TAIL = Op.Operator('tail', 0)
+# NUMBERS = [Op.Operator(str(i), 0) for i in xrange(100)]
+#
+# # make a few variables
+# X = V.Var('x')
+# Y = V.Var('y')
+# Z = V.Var('z')
+#
+# # make an fPCFG for just the minimal list primitives and 0-3
+# list_fpcfg = CFG.FPCFG(primitives={START, LIST, NIL, CONS, DOT,
+#                                    NUMBER, ZERO, ONE, TWO, THREE})
+#
+# # make an fPCFG for list primitives, including head
+# head_fpcfg = CFG.FPCFG(primitives={START, LIST, NUMBER, NIL, NEL, CONS,
+#                                    ZERO, ONE, TWO, THREE, DOT, HEAD},
+#                        start=f(START))
+#
+# # make an fPCFG for list primitives, including head & tail
+# headtail_fpcfg = CFG.FPCFG(primitives={START, LIST, NUMBER, NIL, NEL, CONS,
+#                                        ZERO, ONE, TWO, THREE, DOT, HEAD, TAIL},
+#                            start=f(START))
+#
+# # make a PCFG for learning the syntax of lists
+# rule_START = R.Rule(f(START),
+#                     {f(NUMBER), f(ZERO), f(ONE), f(TWO), f(THREE),
+#                      f(LIST), f(NIL), f(CONS), g(f(START), f(START))})
+#
+# list_pcfg = CFG.PCFG(rules={rule_START})
+#
+# # make a PCFG for the head list problem
+# rule_START = R.Rule(f(START), {f(LIST), f(NUMBER)})
+# rule_LIST = R.Rule(f(LIST), {f(NIL), f(NEL)})
+# rule_NEL = R.Rule(f(NEL), g(g(f(CONS), f(NUMBER)), f(LIST)))
+# rule_NUMBER = R.Rule(f(NUMBER), {f(ZERO), f(ONE), f(TWO), f(THREE)})
+# rule_HEAD = R.Rule(f(NUMBER), g(f(HEAD), f(NEL)))
+#
+# head_pcfg = CFG.PCFG(rules={rule_START, rule_LIST, rule_NEL,
+#                             rule_NUMBER, rule_HEAD},
+#                      start=f(START), locked=False)
+#
+# # make a PCFG for the head/tail problem
+# rule_TAIL = R.Rule(f(LIST), g(f(TAIL), f(LIST)))
+#
+# headtail_pcfg = CFG.PCFG(rules={rule_START, rule_LIST, rule_NEL,
+#                                 rule_NUMBER, rule_HEAD, rule_TAIL},
+#                          start=f(START), locked=False)
+#
+# # make a Grammar for list semantics
+# rule_LIST = R.Rule(f(LIST), {f(NIL), g(g(f(CONS), f(NUMBER)), f(LIST))})
+#
+# list_g = G.Grammar(rules={rule_NUMBER, rule_LIST})
+#
+# # make a Grammar for head semantics
+# head_lhs = g(f(HEAD), g(g(f(CONS), X), Y))
+# head_rhs = X
+# head_rule = R.Rule(head_lhs, head_rhs)
+#
+# head_g = G.Grammar(rules={head_rule}, locked=False)
+#
+# # make a Grammar for head/tail semantics
+# tail_lhs1 = g(f(TAIL), f(NIL))
+# tail_rhs1 = f(NIL)
+# tail_lhs2 = g(f(TAIL), g(g(f(CONS), X), Y))
+# tail_rhs2 = Y
+# rule_TAIL_1 = R.Rule(tail_lhs1, tail_rhs1)
+# rule_TAIL_2 = R.Rule(tail_lhs2, tail_rhs2)
+# headtail_g = G.Grammar(rules={head_rule, rule_TAIL_1, rule_TAIL_2},
+#                        locked=False)
+#
+# # make an LOT for learning to make lists
+# list_lot = LOT.LOT(primitives=list_fpcfg, syntax=list_pcfg, semantics=list_g)
+#
+# # make an LOT for learning head
+# head_lot = LOT.LOT(primitives=head_fpcfg, syntax=head_pcfg, semantics=head_g)
+#
+# # make an LOT for learning head/tail
+# headtail_lot = LOT.LOT(primitives=headtail_fpcfg,
+#                        syntax=headtail_pcfg,
+#                        semantics=headtail_g)
+#
+# # make some test data
+# head_datum_T = R.Rule(g(f(HEAD),
+#                         g(g(f(CONS), f(ZERO)),
+#                           g(g(f(CONS), f(ONE)),
+#                             g(g(f(CONS), f(TWO)),
+#                               g(g(f(CONS), f(THREE)),
+#                                 f(NIL)))))),
+#                       f(ZERO))
+#
+# head_datum_T2 = R.Rule(g(f(HEAD),
+#                          g(g(f(CONS), f(ONE)),
+#                            g(g(f(CONS), f(THREE)),
+#                              g(g(f(CONS), f(TWO)),
+#                                f(NIL))))),
+#                        f(ONE))
+#
+# head_datum_T3 = R.Rule(g(f(HEAD),
+#                          g(g(f(CONS), f(TWO)),
+#                            f(NIL))),
+#                        f(TWO))
+#
+# head_datum_F = R.Rule(g(f(HEAD),
+#                         g(g(f(CONS), f(ZERO)),
+#                           g(g(f(CONS), f(ONE)),
+#                             g(g(f(CONS), f(TWO)),
+#                               g(g(f(CONS), f(THREE)),
+#                                 f(NIL)))))),
+#                       f(NIL))
+#
+# list_datum_T = R.Rule(f(LIST),
+#                       g(g(f(CONS), f(ZERO)),
+#                         g(g(f(CONS), f(ONE)),
+#                           g(g(f(CONS), f(TWO)),
+#                             f(NIL)))))
+#
+# list_datum_F = R.Rule(f(LIST),
+#                       g(g(f(ONE), f(ZERO)),
+#                         f(NIL)))
+#
+# # full head syntax
+# rule_START = R.Rule(f(START), {f(LIST), f(NUMBER)})
+# rule_LIST = R.Rule(f(LIST), {f(NIL), f(NEL)})
+# rule_NEL = R.Rule(f(NEL), g(g(f(CONS), f(NUMBER)), f(LIST)))
+# rule_NUMBER = R.Rule(f(NUMBER), {f(N) for N in NUMBERS})
+# rule_HEAD = R.Rule(f(NUMBER), g(f(HEAD), f(NEL)))
+#
+# full_head_pcfg = CFG.PCFG(rules={rule_START, rule_LIST, rule_NEL,
+#                                  rule_NUMBER, rule_HEAD},
+#                           start=f(START), locked=False)
--- TeRF/Types/TRS.py
+++ TeRF/Types/TRS.py
@@ -1,52 +1,28 @@
-import collections
-import itertools as I
-import numpy as np
-import TeRF.Pseudowords as P
+class TypedTRS(object):
+    """
+    Term Rewriting Systems with Hindley-Milner types
+    """
+    def __init__(self, types, primitives, semantics):
+        """
+        TODO
+        ----
+        - make arguments optional?
+        - are the arguments are consistent with one another (non-zero prob.)
+        - enforce any type constraints on the arguments?
+        """
+        self.types = types
+        self.primitives = primitives
+        self.semantics = semantics
+
+    def __str__(self):
+        return '\n\n'.join([str(self.types),
+                            str(self.primitives),
+                            str(self.semantics)])
 
 
-class TRS(collections.MutableMapping):
-    def save(self, filename):
-        with open(filename, 'w') as f:
-            f.write('signature ')
-            f.write(' '.join(str(op) for op in self.signature) + ';\n')
-            f.write(';\n'.join([str(rule) for rule in self])+';\n')
-
-    def features(self):
-        feats = {}
-        feats['n_rules'] = self.num_rules()
-        feats['n_nodes'] = sum(len(r.lhs) + len(r.rhs0) for r in self.rules())
-        feats['n_lhss'] = len(self)
-        feats['mean_rule_len'] = (float(feats['n_nodes']) /
-                                  float(feats['n_rules']))
-        feats['max_rule_len'] = max(len(r.lhs) + len(r.rhs0)
-                                    for r in self.rules())
-        feats['n_vars'] = sum(1 for r in self.rules()
-                              for t in I.chain(r.lhs.subterms, r.rhs0.subterms)
-                              if not hasattr(t, 'body'))
-        feats['%_vars'] = float(feats['n_vars']) / float(feats['n_nodes'])
-        feats['n_nondet_rules'] = sum(1 for r in self if len(r) > 1)
-        feats['n_nondets'] = sum(len(r) for r in self if len(r) > 1)
-        feats['%_nondets'] = float(feats['n_nondet_rules']) / \
-            float(feats['n_lhss'])
-        return feats
-
-    def rename_operators(self, change=None):
-        if change is not None:
-            to_rename = [op for op in self.signature.operators if op in change]
-            new_names = np.random.choice(P.pseudowords,
-                                         size=len(to_rename),
-                                         replace=False)
-            for op, name in I.izip(to_rename, new_names):
-                for rule in self.rules():
-                    for term in rule.lhs.subterms:
-                        if term.head == op:
-                            term.head.name = name
-                    for term in rule.rhs0.subterms:
-                        if term.head == op:
-                            term.head.name = name
-                op.name = name
-
-    def prettify(self, change=None):
-        self.rename_operators(change)
-        for rule in self:
-            rule.rename_variables()
+if __name__ == '__main__':
+    import TeRF.Test.test_grammars as tg
+    trs = TypedTRS(tg.types,
+                   tg.primitives,
+                   tg.semantics)
+    print trs
