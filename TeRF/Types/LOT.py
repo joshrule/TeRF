4fd86c7d6854a747c936b02a8683434fdb790a33
Josh Rule <rule@mit.edu>
2017-12-06 12:43:11 -0500

    reassign responsibilities between Grammar, LOT, and TRS

--- TeRF/Types/LOT.py
+++ TeRF/Types/LOT.py
@@ -1,13 +1,25 @@
-import TeRF.Types.TypedTRS as TTRS
+class LOT(object):
+    """
+    a representation for Languages of Thought (LOT; Fodor, 1975)
 
+    LOT = first-order Term Rewriting Systems (TRS) + Hindley-Milner typesystem.
+    """
+    def __init__(self, typesystem, trs):
+        """
+        TODO
+        ----
+        - make arguments optional?
+        - make the arguments consistent with one another (non-zero prob.)
+        - enforce any type constraints on the arguments?
+        """
+        self.syntax = typesystem
+        self.semantics = trs
 
-class LOT(TTRS.TypedTRS):
-    pass
+    def __str__(self):
+        return '\n\n'.join([str(self.syntax),
+                            str(self.semantics)])
 
 
 if __name__ == '__main__':
     import TeRF.Test.test_grammars as tg
-    lot = LOT(tg.types,
-              tg.primitives,
-              tg.semantics)
-    print lot
+    print LOT(tg.syntax, tg.semantics)
